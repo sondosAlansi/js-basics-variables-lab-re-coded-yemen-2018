6cac7b920be606f1726d0cb4f4ebe94fb21cb2ea
sondos alansi <sondos.alansi@yahoo.com>
2018-08-23 20:37:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
     let companyName = 'Scuber';
     let mostProfitableNeighborhood='Chelsea';
-    let companyCeo='Susan Smith'
+    let companyCeo='Susan Smith';
